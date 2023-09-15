a18d6a0ed7520d8c5608682c91711f1c5fd5d7f4
Kevin Dyer <kpdyer07@gmail.com>
2023-09-15 01:02:57 -0700

    feat: roll stats
    this new command allows you to roll stats for a new character.
    it takes no options, and displays a list of rolls representing each stat
    
    base roll for a stat is as follows:
    4 keep highest 3 d5

---
+++ bot/commands/roll-stats.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { DiceRoll } = require('@dice-roller/rpg-dice-roller');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roll-stats')
+		.setDescription('Roll for the stats of a new character'),
+	async execute(interaction) {
+    const rollString = '{4d5}kh3';
+    let stats = [];
+    for (let i = 0; i < 6; i +=1) {
+      let roll = new DiceRoll(rollString);
+      stats.push(roll.total);
+    }
+
+		await interaction.reply(`Your new character's stats are: \`\`\`${stats}\`\`\``);
+	},
+};
